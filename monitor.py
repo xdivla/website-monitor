13697c928a76a08609e4872d566be64067f7e873
v.brdar@sportradar.com <v.brdar@sportradar.com>
2024-01-25 11:45:26 +0100

    Updated code - SR

--- monitor.py
+++ monitor.py
@@ -6,15 +6,17 @@ from time import sleep
 from prometheus_client import start_http_server, Gauge
 
 # Prometheus variables
-RESPONSE_CODE = Gauge('website_monitor_response_code', 'Response Status code', ["site", "code"])
-RESPONSE_TIME = Gauge('website_monitor_response_time', 'Response time in seconds', ["site", "status"])
+RESPONSE_CODE = Gauge('endpoint_monitor_response_code', 'Response Status code', ["site"])
+RESPONSE_TIME = Gauge('endpoint_monitor_response_time', 'Response time in seconds', ["site"])
+RESPONSE_STATUS = Gauge('endpoint_monitor_response_status', 'Offline/Online status as 0/1', ["site"])
 
 # Check if the environment variable 'SITES' is defined correctly as an array of URLs
 try:
 	sites = json.loads(os.environ['SITES'])
-except: 
- 	print('Please set the environment variable SITES as an array of URLs: ["https://some_url", "https://some_other_url"]')
- 	quit()
+except Exception as e:
+	print(e)
+	print('Please set the required environment variable SITES as an array of URLs: ["https://some_url", "https://some_other_url"]')
+	quit()
 
 # 'REQUEST_FREQUENCY' environment variable with default value: 30 and illegal value ''
 request_freq = os.environ.get('REQUEST_FREQUENCY', 30)
@@ -23,25 +25,24 @@ if (request_freq == ''):
 	print('The environment variable REQUEST_FREQUENCY cannot be an empty string.')
 	quit()
 
-
 # Defining the event
 def monitor():
 	while True:
 		for site in sites:
 			try:
 				res = requests.get(site, timeout=10)
-			except:
+				RESPONSE_TIME.labels(site=site).set(res.elapsed.total_seconds())
+				RESPONSE_CODE.labels(site=site).set(res.status_code)
+				RESPONSE_STATUS.labels(site=site).set(1)
+			except Exception as e:
+				print(e)
 				print('The site URL ' + site + ' could not be reached or does not exist')
-				RESPONSE_TIME.labels(site=site, status='offline').set(10)
+				RESPONSE_STATUS.labels(site=site).set(0)
 				continue
-
-			RESPONSE_TIME.labels(site=site, status='online').set(res.elapsed.total_seconds())
-			RESPONSE_CODE.labels(site=site, code=res.status_code).set(res.status_code)
 		sleep(int(request_freq))
 
 # Starting the server
 if __name__ == '__main__':
     start_http_server(8888)
+    print('Endpoint monitoring starting... OK')
     monitor()
-    print('Website monitoring started...OK')
-    print('Starting prometheus metrics HTTP server on port 8888...OK')
